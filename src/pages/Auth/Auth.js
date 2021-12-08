b12e8efda2899711f6aad30ad91685320ea6a5c0
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-07 22:39:01 -0300

    user-control-page

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthOptions from "../../components/Auth/AuthOptions/AuthOptions";
+import LoginForm from "../../components/Auth/LoginForm/LoginForm";
+import RegisterForm from "../../components/Auth/RegisterForm";
+
+import "./Auth.scss";
+
+import BackgroundAuth from "../../assets/jpg/background-auth.jpg";
+import LogoNameWhite from "../../assets/png/logo-name-white.png";
 
 export default function Auth() {
+    const [selectedForm, setSelectedForm] = useState(null);
+
+    const formHandler = () => {
+        switch (selectedForm) {
+            case "login":
+                return <LoginForm />
+            case "register":
+                return <RegisterForm />
+            default:
+                return <AuthOptions setSelectedForm={setSelectedForm} />
+        }
+    }
+
     return (
-        <div>
-            <h2>Iniciar Sesion</h2>
-            <h2>Registrarse</h2>
+        <div className="auth" style={{ backgroundImage: `url(${BackgroundAuth})` }}>
+            <div className="auth__dark" />
+            <div className="auth__box">
+                <div className="auth__box-logo">
+                    <img src={LogoNameWhite} alt="Musicfy" />
+                </div>
+                {formHandler()}
+
+            </div>
         </div>
+
     )
 }
