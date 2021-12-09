36a3fdbd2d98ad598c2c1827ba583f2bbf8aa280
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-08 21:09:07 -0300

    validating-form

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -3,12 +3,16 @@ import { Button, Icon, Form, Input } from 'semantic-ui-react';
 import firebaseApp from "../../../utils/firebase";
 import { getAuth } from "firebase/auth";
 
+
+import { validateEmail } from '../../../utils/Validations';
 import './RegisterForm.scss';
 
 export default function RegisterForm(props) {
     const { setSelectedForm } = props;
     const [formData, setFormData] = useState(defaultValueForm);
     const [showPassword, setShowPassword] = useState(false);
+    const [formError, setFormError] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     const showPasswordHandler = () => {
         setShowPassword(!showPassword);
@@ -22,8 +26,30 @@ export default function RegisterForm(props) {
     }
 
     const onSubmitHandler = () => {
-        console.log('Formulario enviado');
-        console.log(formData)
+        setFormError({});
+        let errors = {};
+        let formOk = true;
+
+        if (!validateEmail(formData.email)) {
+            errors.email = true;
+            formOk = false;
+        }
+
+        if (formData.password.length < 6) {
+            errors.password = true;
+            formOk = false;
+        }
+
+        if (!formData.username) {
+            errors.username = true;
+            formOk = false;
+        }
+
+        setFormError(errors);
+
+        if (formOk) {
+            console.log("Formulario VALIDO");
+        }
     }
     return (
         <div className="register-form">
@@ -35,8 +61,13 @@ export default function RegisterForm(props) {
                         name="email"
                         placeholder="Correo electronico"
                         icon="mail outline"
-                    //error={}
+                        error={formError.email}
                     />
+                    {formError.email && (
+                        <span className="error-text">
+                            Por favor, introduce un email valido
+                        </span>
+                    )}
                 </Form.Field>
                 <Form.Field>
                     <Input
@@ -48,8 +79,13 @@ export default function RegisterForm(props) {
                         ) : (
                             <Icon name="eye" link onClick={showPasswordHandler} />
                         )}
-                    //error={}
+                        error={formError.password}
                     />
+                    {formError.password && (
+                        <span className="error-text">
+                            Por favor, elige una constraseña superior a 5 caracteres
+                        </span>
+                    )}
                 </Form.Field>
                 <Form.Field>
                     <Input
@@ -57,8 +93,13 @@ export default function RegisterForm(props) {
                         name="username"
                         placeholder="Como deberiamos llamarte?"
                         icon="user circle outline"
-                    //error={}
+                        error={formError.username}
                     />
+                    {formError.username && (
+                        <span className="error-text">
+                            Por favor, introduce un nombre
+                        </span>
+                    )}
                 </Form.Field>
                 <Button type="submit">Continuar</Button>
             </Form>
