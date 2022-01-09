9ba6f890fcea7911d869607754e4299e6206ed24
Antonio Sucre <ajs21br@gmail.com>
2022-01-09 17:41:21 -0300

    Mostrar Item de Menu Activo

--- src/components/MenuLeft/MenuLeft.js
+++ src/components/MenuLeft/MenuLeft.js
@@ -7,13 +7,30 @@ import "./MenuLeft.scss";
 function MenuLeft(props) {
     const { user } = props;
 
+    const [activeMenu, setActiveMenu] = useState("/");
+
+    const handlerMenu = (e, menu) => {
+        setActiveMenu(menu.to);
+    }
+
+
     return (
         <Menu className='menu-left' vertical>
             <div className='top'>
-                <Menu.Item as={Link} to="/" name='home'>
+                <Menu.Item
+                    as={Link}
+                    to="/"
+                    active={activeMenu === "/"}
+                    onClick={handlerMenu}
+                >
                     <Icon name='home' /> Inicio
                 </Menu.Item>
-                <Menu.Item as={Link} to="/artists" name='artists'>
+                <Menu.Item
+                    as={Link}
+                    to="/artists"
+                    active={activeMenu === "/artists"}
+                    onClick={handlerMenu}
+                >
                     <Icon name='music' /> Artistas
                 </Menu.Item>
             </div>
