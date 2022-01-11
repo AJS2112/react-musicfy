eebf531e328d5d27e6cc7d9a1c335ab9783879cd
Antonio Sucre <ajs21br@gmail.com>
2022-01-10 21:52:50 -0300

    Formulario para actualizar email usuario

--- src/components/Settings/UserEmail.js
+++ src/components/Settings/UserEmail.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Form, Input, Button } from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Form, Input, Button, Icon } from "semantic-ui-react";
 
 
 export default function UserEmail(props) {
     const { user, setShowModal, setTitleModal, setContentModal } = props;
 
     const onEdit = () => {
-        console.log('Editando email');
+        setTitleModal("Actualizar Email");
+        setContentModal(<ChangeEmailForm user={user} setShowModal={setShowModal} />);
+        setShowModal(true);
     }
 
     return (
         <div className='user-email'>
             <h3>Email: {user.email}</h3>
             <Button circular onClick={onEdit}>
-        Actualizar
+                Actualizar
             </Button>
         </div>
+    )
+}
+
+function ChangeEmailForm(props) {
+    const { user, setShowModal } = props;
+    const { email } = user;
+    const [formData, setFormData] = useState({ email: email });
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = () => {
+        console.log('cambiando email')
+    }
+
+    return (
+        <Form onSubmit={onSubmit}>
+            <Form.Field>
+                <Input
+                    defaultValue={email}
+                    onChange={e => setFormData({ email: e.target.value })}
+                />
+            </Form.Field>
+            <Form.Field>
+                <Input
+                    placeholder="contraseña"
+                    type="password"
+                    icon={<Icon name="eye" link />}
+                //onChange={e => setFormData({ email: e.target.value })}
+                />
+            </Form.Field>
+            <Button type="submit" loading={isLoading}>
+                Actualizar email
+            </Button>
+        </Form>
     )
 }
