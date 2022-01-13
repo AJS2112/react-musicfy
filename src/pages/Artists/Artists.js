1c15e899cb73b6732d55a20a5eea1db244fe55e2
Antonio Sucre <ajs21br@gmail.com>
2022-01-13 19:05:41 -0300

    Obteniendo los artistas de firebase

--- src/pages/Artists/Artists.js
+++ src/pages/Artists/Artists.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebaseApp from "../../utils/firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { map } from "lodash";
 
 import "./Artists.scss";
 
+const db = getFirestore(firebaseApp);
+
 export default function Artists() {
+    const [artists, setArtists] = useState([]);
+
+    console.log(artists);
+
+    useEffect(() => {
+        getDocs(collection(db, "artists"))
+            .then(response => {
+                const arrayArtists = [];
+                map(response?.docs, artist => {
+                    const data = artist.data();
+                    data.id = artist.id;
+                    arrayArtists.push(data);
+                });
+                setArtists(arrayArtists);
+            });
+    }, [])
+
     return (
         <div className="artists">
             <h1>Artistas</h1>
