a9c03d88e79f91a57f374ae4a91ab031c45fd8a6
Antonio Sucre <ajs21br@gmail.com>
2022-01-13 19:28:54 -0300

    Obteniendo imagen de los artistas

--- src/pages/Artists/Artists.js
+++ src/pages/Artists/Artists.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
 import firebaseApp from "../../utils/firebase";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
 import { map } from "lodash";
 
 import "./Artists.scss";
@@ -10,7 +13,7 @@ const db = getFirestore(firebaseApp);
 export default function Artists() {
     const [artists, setArtists] = useState([]);
 
-    console.log(artists);
+    //console.log(artists);
 
     useEffect(() => {
         getDocs(collection(db, "artists"))
@@ -28,6 +31,33 @@ export default function Artists() {
     return (
         <div className="artists">
             <h1>Artistas</h1>
+            <Grid>
+                {map(artists, artist => (
+                    <Artist key={artist.id} artist={artist} />
+                ))}
+            </Grid>
+        </div>
+    )
+}
+
+function Artist(props) {
+    const { artist } = props;
+    const [bannerUrl, setBannerUrl] = useState(null);
+
+    useEffect(() => {
+        var storage = getStorage(firebaseApp);
+        var storageRefence = ref(storage, `artist/${artist.banner}`);
+        getDownloadURL(storageRefence)
+            .then(url => {
+                setBannerUrl(url);
+            }).catch((err) => {
+                console.log(err)
+            })
+    }, [artist]);
+
+    return (
+        <div>
+            <h2>{artist.name}</h2>
         </div>
     )
 }
