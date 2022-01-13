5d7dcfad51e317b6e1c1e9a8d91cf796eae5a1f2
Antonio Sucre <ajs21br@gmail.com>
2022-01-13 20:23:53 -0300

    Añadir useDropzone para el avatar del album

--- src/components/Album/AddAlbumForm/AddAlbumForm.js
+++ src/components/Album/AddAlbumForm/AddAlbumForm.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Form, Input, Button, Image, Dropdown } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
+
+import NoImage from "../../../assets/png/no-image.png";
 
 import './AddAlbumForm.scss';
 
 export default function AddAlbumForm(props) {
     const { setShowModal } = props;
+    const [albumImage, setAlbumImage] = useState(null);
+    const [file, setFile] = useState(null)
+
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
+        setFile(file);
+        setAlbumImage(URL.createObjectURL(file));
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        onDrop
+    });
 
     const onSubmit = () => {
         console.log("enviando formulario");
@@ -14,7 +31,15 @@ export default function AddAlbumForm(props) {
         <Form className="add-album-form" onSubmit={onSubmit}>
             <Form.Group>
                 <Form.Field className="album-avatar" width={5}>
-                    <h2>Avatar</h2>
+                    <div
+                        {...getRootProps()}
+                        className="avatar"
+                        style={{
+                            backgroundImage: `url('${albumImage}')`
+                        }}
+                    />
+                    <input {...getInputProps()} />
+                    {!albumImage && <Image src={NoImage} />}
                 </Form.Field>
                 <Form.Field className="album-inputs" width={11}>
                     <Input placeholder="Nombre del album" />
