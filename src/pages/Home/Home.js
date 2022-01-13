05cec226f5c82eb6780921be86aea1e0254ff3b7
Antonio Sucre <ajs21br@gmail.com>
2022-01-12 22:27:54 -0300

    Obteniendo todos los artistas de firebase

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BannerHome from "../../components/BannerHome";
-
+import firebaseApp from "../../utils/firebase";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { map } from "lodash";
 import "./Home.scss";
 
+const db = getFirestore(firebaseApp);
+
 export default function Home() {
+    const [artists, setArtists] = useState([]);
+
+    console.log(artists);
+
+    useEffect(() => {
+        getDocs(collection(db, "artists"))
+            .then(response => {
+                const arrayArtists = [];
+                map(response?.docs, artist => {
+                    const data = artist.data();
+                    data.id = artist.id;
+                    arrayArtists.push(data);
+                });
+                setArtists(arrayArtists);
+            })
+
+    }, [])
+
     return (
         <>
             <BannerHome />
