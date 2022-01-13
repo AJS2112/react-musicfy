c49db9e5baa14519427a5a8467954bf89b60d2f4
Antonio Sucre <ajs21br@gmail.com>
2022-01-13 08:59:07 -0300

    Obteniendo informacion del artista seleccionado

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import firebaseApp from "../../utils/firebase";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 import "./Artist.scss";
 
+const db = getFirestore(firebaseApp);
+
 function Artist(props) {
+    const { match } = props;
+    const [artist, setArtist] = useState(null);
+
+    useEffect(() => {
+
+        getDoc(doc(db, "artists", match?.params?.id))
+            .then(response => {
+                setArtist(response.data());
+            })
+
+    }, [match])
+
+
+
     return (
         <div>
             <h1>
