ea607d8111b263ec9257ba9351345458daf6c618
Antonio Sucre <ajs21br@gmail.com>
2022-01-14 06:39:50 -0300

    Mostrando la cancion

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,12 +3,20 @@ import { Grid, Progress, Icon, Input, Image } from "semantic-ui-react";
 
 import "./Player.scss";
 
-export default function Player() {
+export default function Player(props) {
+    //const { songData } = props;
+
+    const songData = {
+        image: 'https://firebasestorage.googleapis.com/v0/b/musicfy-dbac4.appspot.com/o/album%2F77bdafba-2890-444a-a744-665669e4a0d9?alt=media&token=5c9b4186-c9ce-4fd3-a45b-47167d00bae1',
+        name: "Hypersonic"
+    }
+
     return (
         <div className="player">
             <Grid>
                 <Grid.Column width={4} className="left">
-                    <h2>left</h2>
+                    <Image src={songData?.image} />
+                    {songData?.name}
                 </Grid.Column>
                 <Grid.Column width={8} className="center">
                     <h2>center</h2>
