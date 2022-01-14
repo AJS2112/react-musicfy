d455d3e252ddc8b9d336542335fda9d7dd8c15bb
Antonio Sucre <ajs21br@gmail.com>
2022-01-14 05:30:53 -0300

    Obteniendo la url del album

--- src/pages/Album/Album.js
+++ src/pages/Album/Album.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import firebaseApp from "../../utils/firebase";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 import './Album.scss';
 
@@ -10,14 +11,27 @@ const db = getFirestore(firebaseApp);
 function Album(props) {
     const { match } = props;
     const [album, setAlbum] = useState(null);
+    const [albumImg, setAlbumImg] = useState(null);
 
     useEffect(() => {
         getDoc(doc(db, "albums", match?.params?.id))
             .then(response => {
                 setAlbum(response.data());
             })
-
     }, [match])
+
+    useEffect(() => {
+        if (album) {
+            var storage = getStorage(firebaseApp);
+            var storageRefence = ref(storage, `album/${album?.banner}`);
+            getDownloadURL(storageRefence)
+                .then(url => {
+                    setAlbumImg(url);
+                }).catch((err) => {
+                    console.log(err)
+                })
+        }
+    }, [album]);
 
     return (
         <div>
