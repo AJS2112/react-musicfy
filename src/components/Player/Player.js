3a2001e5bee809abc0cd9d09234045b7ed276f61
Antonio Sucre <ajs21br@gmail.com>
2022-01-14 06:32:40 -0300

    Creando estructura del player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Grid, Progress, Icon, Input, Image } from "semantic-ui-react";
 
 import "./Player.scss";
 
 export default function Player() {
     return (
-        <div>
-            <h3>Player...</h3>
+        <div className="player">
+            <Grid>
+                <Grid.Column width={4} className="left">
+                    <h2>left</h2>
+                </Grid.Column>
+                <Grid.Column width={8} className="center">
+                    <h2>center</h2>
+                </Grid.Column>
+                <Grid.Column width={4} className="right">
+                    <h2>right</h2>
+                </Grid.Column>
+            </Grid>
         </div>
     )
 }
