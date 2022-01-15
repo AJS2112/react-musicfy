04b618faa036f358e7f0aee30c9c5fc407d14a31
Antonio Sucre <ajs21br@gmail.com>
2022-01-15 10:38:02 -0300

    Creando slider de canciones

--- src/components/Sliders/SongsSlider/SongsSlider.js
+++ src/components/Sliders/SongsSlider/SongsSlider.js
@@ -1,14 +1,44 @@
 import React from "react";
+import Slider from "react-slick";
+import { size, map } from "lodash";
 
 import "./SongsSlider.scss";
 
 export default function SongsSlider(props) {
     const { title, data } = props;
 
+    const settings = {
+        dots: false,
+        infinite: true,
+        autoplay: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: true,
+        className: "songs-slider__list"
+    }
+
+    // if (size(data)<5) {
+    //     return null;
+    // }
+
     return (
         <div className="songs-slider">
             <h2>{title}</h2>
-            <p>Slider...</p>
+            <Slider {...settings}>
+                {map(data, item => (
+                    <Song key={item.id} item={item} />
+                ))}
+            </Slider>
         </div>
+    )
+}
+
+function Song(props) {
+    const { item } = props;
+    console.log(item)
+    return (
+
+        <p>{item.name}</p>
     )
 }
