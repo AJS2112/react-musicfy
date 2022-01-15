9e271f81c6ab41741bafec338553d367ed926751
Antonio Sucre <ajs21br@gmail.com>
2022-01-15 10:51:03 -0300

    Obteniendo imagen del album de la cancion

--- src/components/Sliders/SongsSlider/SongsSlider.js
+++ src/components/Sliders/SongsSlider/SongsSlider.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import { size, map } from "lodash";
+import { Link } from 'react-router-dom';
+import firebaseApp from "../../../utils/firebase";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { getFirestore, collection, getDoc, doc } from "firebase/firestore";
 
 import "./SongsSlider.scss";
+
+const db = getFirestore(firebaseApp);
+
 
 export default function SongsSlider(props) {
     const { title, data } = props;
@@ -36,7 +43,35 @@ export default function SongsSlider(props) {
 
 function Song(props) {
     const { item } = props;
-    console.log(item)
+
+    const [banner, setBanner] = useState(null);
+    const [album, setAlbum] = useState(null);
+
+    console.log(album)
+
+    useEffect(() => {
+
+        getDoc(doc(db, "albums", item.album))
+            .then(response => {
+                const data = response.data();
+                data.id = response.id;
+                setAlbum(data);
+                getImage(data);
+            })
+
+    }, [item]);
+
+    const getImage = album => {
+        var storage = getStorage(firebaseApp);
+        var storageRefence = ref(storage, `album/${album.banner}`);
+        getDownloadURL(storageRefence)
+            .then(url => {
+                setBanner(url);
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
+
     return (
 
         <p>{item.name}</p>
